e9be71bd920efa1b9eb4546eb7538b8f98c7c3c7
mopangei <natita.susu@gmail.com>
2026-03-20 23:03:26 +0700

    Menu toggle animation

--- ChickongChicken_TD/Assets/Scripts/Menu.cs
+++ ChickongChicken_TD/Assets/Scripts/Menu.cs
@@ -7,8 +7,17 @@ public class Menu : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] TextMeshProUGUI currencyUI;
+    [SerializeField] Animator anim;
 
-    private void Update()
+    private bool isMenuOpen = true;
+
+    public void ToggleMenu()
+    {
+        isMenuOpen = !isMenuOpen;
+        anim.SetBool("MenuOpen", isMenuOpen);
+    }
+
+    private void OnGUI()
     {
         currencyUI.text = LevelManager.main.currency.ToString();
     }
