a67463e8755574781691aea851d98648abf897d9
jch2bbb <jidapa.ch2@student.mahidol.edu>
2026-03-24 00:01:45 +0700

    Scene Decor Update

--- ChickongChicken_TD/Assets/Scripts/StageButton.cs
+++ ChickongChicken_TD/Assets/Scripts/StageButton.cs
@@ -7,20 +7,23 @@ public class StageButton : MonoBehaviour
 {
     [Header("Popup Panel")]
     [SerializeField] private GameObject popupPanel;
-    [SerializeField] private TextMeshProUGUI popupTitle;
+    [SerializeField] private TextMeshProUGUI popupTitle_1;
+    [SerializeField] private TextMeshProUGUI popupTitle_2;
     [SerializeField] private TextMeshProUGUI popupDescription;
     [SerializeField] private GameObject blackBG;
 
 
     [Header("This Stage's Data")]
-    [SerializeField] private string stageName;
+    [SerializeField] private string stageName_1;
+    [SerializeField] private string stageName_2;
     [SerializeField] private string stageDescription;
 
     public void OpenPopup()
     {
         if (popupPanel != null)
         {
-            popupTitle.text = stageName;
+            popupTitle_1.text = stageName_1;
+            popupTitle_2.text = stageName_2;
             popupDescription.text = stageDescription;
             popupPanel.SetActive(true);
             blackBG.SetActive(true);
