ae9cd12c73fafd83d6df664ae8f1ec8357a26fc8
jch2bbb <jidapa.ch2@student.mahidol.edu>
2026-03-29 18:18:36 +0700

    Tutorial (Unfinished)

--- ChickongChicken_TD/Assets/Scripts/Tutorial.cs
+++ ChickongChicken_TD/Assets/Scripts/Tutorial.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class EnemySpawner : MonoBehaviour
+public class Tutorial : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private GameObject[] enemyPrefabs;
